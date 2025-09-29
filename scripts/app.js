6876d51e2ec49c28799ddf7fe3c968307268bab5
Max Chu <maxchu2021@gmail.com>
2025-09-29 20:28:35 +0800

    更新

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,10 @@
 function scroll() {
-    if (window.scrollY > document.querySelector('section').offsetHeight * 8 - 300) {
+    if (document.querySelector('.contact').getBoundingClientRect().y < 100) {
         document.querySelector('.scroll').innerHTML = '<span class="line">___</span> Scroll';
         document.querySelector('.scroll').style.marginRight = '0';
     } else if (window.scrollY > document.querySelector('section').offsetHeight - 300) {
         document.querySelector('.scroll').innerHTML = '<span class="line">___</span> Scroll <span class="line">___</span>';
-        document.querySelector('.scroll').style.marginRight = '-30px';
+        document.querySelector('.scroll').style.marginRight = '-34px';
     } else {
         document.querySelector('.scroll').innerHTML = 'Scroll <span class="line">___</span>';
         document.querySelector('.scroll').style.marginRight = '0';
