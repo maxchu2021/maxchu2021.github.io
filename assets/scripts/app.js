2b6cca98691c047055095cf7d2ee51dcce712c34
Max Chu <maxchu2021@gmail.com>
2025-10-17 19:12:10 +0800

    修正glide

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -18,9 +18,6 @@ window.addEventListener('scroll', () => {
 scroll();
 
 new Glide('.glide', { autoplay: 2000 }).mount()
-new Glide('.glide2', { autoplay: 2000 }).mount()
-new Glide('.glide3', { autoplay: 2000 }).mount()
-new Glide('.glide4', { autoplay: 2000 }).mount()
 
 // Add scroll animation for about section
 const aboutSection = document.querySelector('#about');
